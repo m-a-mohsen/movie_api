0f05c4b93fcdd04fd131814e99dfe5532a3d93c7
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2022-11-15 01:50:30 +0100

    2.4 final

---
+++ index.js
@@ -0,0 +1,46 @@
+
+//imports
+const express = require('express'),
+
+    morgan = require('morgan');
+
+// app initialisation
+const app = express();
+
+// data
+let topMovies = [
+    {
+        title: 'Die hard',
+        releaseDate: 1990
+    },
+    {
+        title: 'Lord of the Rings',
+        releaseDate: 2001
+    },
+    {
+        title: 'Twilight',
+        releaseDate: 2010
+    }
+];
+
+// Middleware
+app.use(express.static('public'));
+app.use(morgan('common'));
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// GET requests
+app.get('/', (req, res) => {
+    res.send('Welcome to Movies database!');
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+//listener
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
