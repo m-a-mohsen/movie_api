5bd1da0eeea2bc62d25bb7f11b451980428b4298
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2022-12-04 05:35:56 +0100

    Final 2.8

---
+++ models.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+// --------- Schemas -------------
+let movieSchema = mongoose.Schema({
+    Title : {type : String, required: true},
+    Description : {type : String, required : true},
+    Genre : {
+        Name : String,
+        Description : String
+    },
+    Director : {
+        Name : String,
+        Bio : String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+    UserName: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  });
+
+// ------ models ---------
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+
+// ----- exports -----------
+module.exports.Movie = Movie;
+module.exports.User = User;
