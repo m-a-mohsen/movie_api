cf70b184faf4707230601e2c303174ffe4a5a4d2
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2022-12-10 10:33:27 +0100

    final

--- models.js
+++ models.js
@@ -1,33 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // --------- Schemas -------------
-let movieSchema = mongoose.Schema({
-    Title : {type : String, required: true},
-    Description : {type : String, required : true},
-    Genre : {
-        Name : String,
-        Description : String
-    },
-    Director : {
-        Name : String,
-        Bio : String
-    },
-    Actors: [String],
-    ImagePath: String,
-    Featured: Boolean
+const movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String,
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean,
 });
 
-let userSchema = mongoose.Schema({
-    UserName: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-  });
+const userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
+});
 
 // ------ models ---------
-let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User', userSchema);
-
+const Movie = mongoose.model("Movie", movieSchema);
+const User = mongoose.model("User", userSchema);
 
 // ----- exports -----------
 module.exports.Movie = Movie;
