f8bc5eed970dc7e8d8afc5cce800e11848123b41
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-04-26 14:00:22 +0200

    try catch

--- passport.js
+++ passport.js
@@ -14,28 +14,42 @@ passport.use(
       usernameField: "Username",
       passwordField: "Password",
     },
-    (username, password, callback) => {
-      console.log(`${username}  ${password}`);
-      Users.findOne({ Username: username }).then((error, user) => {
-        if (error) {
-          console.log(error);
-          return callback(error);
-        }
-
+    async (username, password, callback) => {
+      try {
+        const user = await Users.findOne({ Username: username });
         if (!user) {
           console.log("incorrect username");
           return callback(null, false, { message: "Incorrect username." });
         }
-
         if (!user.validatePassword(password)) {
           console.log("incorrect password");
           return callback(null, false, { message: "Incorrect password." });
         }
-
         console.log("finished");
         return callback(null, user);
-      });
+      } catch (error) {
+        console.log(error);
+        return callback(error);
+      }
+      // console.log(`${username}  ${password}`);
+      // Users.findOne({ Username: username }, (error, user) => {
+      //   if (error) {
+      //     console.log(error);
+      //     return callback(error);
+      //   }
+      //   if (!user) {
+      //     console.log("incorrect username");
+      //     return callback(null, false, { message: "Incorrect username." });
+      //   }
+      //   if (!user.validatePassword(password)) {
+      //     console.log("incorrect password");
+      //     return callback(null, false, { message: "Incorrect password." });
+      //   }
+      //   console.log("finished");
+      //   return callback(null, user);
+      // });
     }
+    // end of function
   )
 );
 
